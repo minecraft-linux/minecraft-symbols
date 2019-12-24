0c9ec69250d8a4ea474d069b474cc6f644135e69
MCMrARM <mcmrarm@gmail.com>
2019-12-24 22:23:33 +0100

    Restore legacy Core::Path constructors This fixes server building

--- include/minecraft/legacy/Core.h
+++ include/minecraft/legacy/Core.h
@@ -25,6 +25,16 @@ namespace Core {
 
 struct Path {
 
+    Path(const std::string & s) {
+        path = s.c_str();
+        size = s.length();
+    }
+
+    Path(const mcpe::string & s) {
+        path = s.c_str();
+        size = s.length();
+    }
+
     const char *path;
     size_t size;
 
